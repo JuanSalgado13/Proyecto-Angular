3067fcfa59e69ed7b8dbe1b9cfd78233c9fa18d9
JuanSalgado13 <salgadomartinez2111@gmail.com>
2025-09-08 16:38:09 -0500

    first actualizaciones

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
 import { NavComponent } from '../shared/component/nav/nav.component';
 import { FooterComponent } from '../shared/component/footer/footer.component';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
-  imports: [NavComponent,FooterComponent],
+  imports: [NavComponent, FooterComponent],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
 })
 export class HomeComponent {
-
+  constructor(private router: Router) {}
+  irAPagina() {
+    this.router.navigate(['https://ico.org/es/']); // Navega a la página de contacto
+  }
 }
+
