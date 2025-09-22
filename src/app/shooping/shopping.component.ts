9815cb955b697af9fbb8285b89580cd8fc291622
JuanSalgado13 <salgadomartinez2111@gmail.com>
2025-09-22 16:18:35 -0500

    agg 22/09/25 v3

--- src/app/shooping/shopping.component.ts
+++ src/app/shooping/shopping.component.ts
@@ -1,18 +1,46 @@
-import { Component, signal } from '@angular/core';
-import { Router } from '@angular/router';
+import { Component, AfterViewInit, OnDestroy, ElementRef } from '@angular/core';
+import gsap from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
 import { NavComponent } from '../shared/component/nav/nav.component';
 import { FooterComponent } from '../shared/component/footer/footer.component';
-import { Producto, CafesService } from '../services/cafes.service';
-import { CommonModule, CurrencyPipe } from '@angular/common';
 
-
+gsap.registerPlugin(ScrollTrigger);
 
 @Component({
   selector: 'app-shopping',
-  imports: [CommonModule, NavComponent, FooterComponent],
   templateUrl: './shopping.component.html',
-  styleUrl: './shopping.component.css',
+  imports: [NavComponent,FooterComponent],
+  styleUrls: ['./shopping.component.css']
 })
-export class ShoppingComponent {
- 
+export class ShoppingComponent implements AfterViewInit, OnDestroy {
+  private ctx!: gsap.Context;
+
+  constructor(private el: ElementRef) {}
+
+  ngAfterViewInit(): void {
+    
+    this.ctx = gsap.context(() => {
+      
+      gsap.utils.toArray<HTMLElement>('.contain').forEach(section => {
+        gsap.from(section.querySelectorAll('.text, .deptos'), {
+          y: 60,
+          opacity: 0,
+          duration: 1,
+          stagger: 0.3,
+          ease: 'power2.out',
+          scrollTrigger: {
+            trigger: section,
+            start: 'top 60%', 
+            toggleActions: 'play none none reverse'
+            
+          }
+        });
+      });
+    }, this.el.nativeElement);
+  }
+
+  ngOnDestroy(): void {
+    // Limpia animaciones y ScrollTriggers
+    this.ctx?.revert();
+  }
 }
